73ecc279299058a1f4529beaf0250ba770955cc1
akmal <akmal@akmals-MacBook-Pro.local>
2026-06-02 01:06:27 +0700

    fix audio

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2413131701" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2434867280" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
