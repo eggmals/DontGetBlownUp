504d383cbb2c85b5b9c1f71b36c2b0f7cf6dafe6
akmal <akmal@akmals-MacBook-Pro.local>
2026-06-01 15:19:27 +0700

    fix base href

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2425405477" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "758230564" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
